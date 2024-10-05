3ef4ce34bdaa3c773e3487b428f6a67b88784c21
pragati243 <118956494+pragati243@users.noreply.github.com>
2024-10-05 13:56:44 +0530

    Update route.ts

--- app/api/loan-applications/route.ts
+++ app/api/loan-applications/route.ts
@@ -1,39 +1,49 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse, NextRequest } from "next/server";
-import mongoose from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
 import LoanRequest from "@/models/LoanRequest";
 import { mongoConnect } from "@/config/mongodb";
 
 export async function POST(request: NextRequest) {
   const reqBody = await request.json();
 
-    if (!process.env.MONGODB_URI) {
-    return  NextResponse.json({ message: "MONGODB_URI not configured in .env" }, { status: 500 });
-
+  if (!process.env.MONGODB_URI) {
+    return NextResponse.json(
+      { message: "MONGODB_URI not configured in .env" },
+      { status: 500 }
+    );
   }
+
   await mongoConnect();
 
-  const loanRequest = new LoanRequest({ ...reqBody, loanstatus: 'pending' }); // Set the status to 'pending'
+  const loanRequest = new LoanRequest({ ...reqBody, loanstatus: "pending" });
 
-  console.log('Loan Request before saving:', loanRequest);
+  console.log("Loan Request before saving:", loanRequest);
 
   try {
     await loanRequest.save();
-    console.log('Loan Request after saving:', loanRequest);
-    return NextResponse.json({
-      message: "Loan application submitted successfully!",
-      id: loanRequest._id,
-      loanstatus: loanRequest.loanstatus,
-    });
+    console.log("Loan Request after saving:", loanRequest);
+    return NextResponse.json(
+      {
+        message: "Loan application submitted successfully!",
+        id: loanRequest._id,
+        loanstatus: loanRequest.loanstatus,
+      },
+      { status: 201 }
+    );
   } catch (error) {
-    console.error('Error saving loan request:', error);
-    return NextResponse.error(error);
+    console.error("Error saving loan request:", error);
+    return NextResponse.json(
+      { message: "Error saving loan request" },
+      { status: 500 }
+    );
   }
 }
 
 export async function GET(request: NextRequest) {
   if (!process.env.MONGODB_URI) {
-    return NextResponse.status(500).json({ message: "MONGODB_URI not configured in .env" });
+    return NextResponse.json(
+      { message: "MONGODB_URI not configured in .env" },
+      { status: 500 }
+    );
   }
 
   await mongoConnect();
@@ -43,7 +53,9 @@ export async function GET(request: NextRequest) {
     return NextResponse.json(loanRequests);
   } catch (error) {
     console.error("Error fetching loan requests:", error);
-    return NextResponse.status(500).json({ message: "Error fetching loan requests" });
+    return NextResponse.json(
+      { message: "Error fetching loan requests" },
+      { status: 500 }
+    );
   }
-  
 }
